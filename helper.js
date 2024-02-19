4f3f7310d2169bf970ddd243a7275ecb36e4e22d
Dharmik Thakkar <heavynotebook@gmail.com>
2024-02-19 21:00:13 +0530

    added few helper function

--- helper.js
+++ helper.js
@@ -5,3 +5,9 @@ function addtion(int num1,int num2){
 function substation(int num1,int num2){
     return num1-num2;
 }
+function mutiply(int num1,int num2){
+    return num1*num2;
+}
+function divide(int num1,int num2){
+    return num1%num2;
+}
