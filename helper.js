e1de3a145d40380b7cb7b713c6c11b1c01ddcae1
Dharmik Thakkar <heavynotebook@gmail.com>
2024-02-20 08:08:10 +0530

    Revert "added the helper funtion"
    This reverts commit 759b285b17e615ea1fd4c0ce72da45ba3335f24a.

--- helper.js
+++ helper.js
@@ -1,16 +1,3 @@
 function addtion(int num1,int num2){
     return num1+num2;
-<<<<<<< HEAD
-}
-
-function substation(int num1,int num2){
-    return num1-num2;
-}
-function mutiply(int num1,int num2){
-    return num1*num2;
-}
-function divide(int num1,int num2){
-    return num1%num2;
-=======
->>>>>>> parent of 759b285 (added the helper funtion)
 }
